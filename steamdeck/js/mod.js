a58ca540bae9fa11d06b7e30e5bb8771eaa3e69a
Aaron Maynard <aaronmaynard42@gmail.com>
2022-11-09 01:59:10 -0600

    Added Settings Overlay

--- steamdeck/js/mod.js
+++ steamdeck/js/mod.js
@@ -1,7 +1,14 @@
-function connectClient() {
-	document.getElementById("setupOverlay").style.height = "100%";
+function connectClientOpen() {
+	document.getElementById("connectClient").style.height = "100%";
 }
 
 function connectClientClose() {
-	document.getElementById("setupOverlay").style.height = "0%";
+	document.getElementById("connectClient").style.height = "0%";
+}
+function settingsOpen() {
+	document.getElementById("settings").style.height = "100%";
+}
+
+function settingsClose() {
+	document.getElementById("settings").style.height = "0%";
 }
